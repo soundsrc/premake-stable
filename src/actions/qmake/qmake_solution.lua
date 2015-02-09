c214b60d2ae2c23168cb83c620b8695646f0ff9b
Sound <sound@sagaforce.com>
2015-02-09 11:24:50 -0800

    Generate qmake projects with explicit dependancies.

--- src/actions/qmake/qmake_solution.lua
+++ src/actions/qmake/qmake_solution.lua
@@ -1,9 +1,16 @@
 
 function premake.qmake.solution(sln)
 	_p(0, 'TEMPLATE = subdirs')
-	_p(0, 'SUBDIRS += \\')
+	
 	for prj in premake.solution.eachproject(sln) do
-		_p('%s.pro \\', prj.name)
+		_p(0, 'SUBDIRS += %s', prj.name)
+		_p(1, '%s.file += %s.pro', prj.name, prj.name)
+		local deps = premake.getdependencies(prj)
+		if #deps > 0 then
+			for _, depprj in ipairs(deps) do
+				_p(1, '%s.depends += %s', prj.name, depprj.name)
+			end
+		end
 	end
 	_p(0, '')
 end
